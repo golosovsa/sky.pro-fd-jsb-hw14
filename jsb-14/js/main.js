d21b7cd81f8320d12e7243c8074497395d44fb0e
grm <golosov_SA@mail.ru>
2022-08-16 16:32:07 +0300

    Task 1 Done

---
+++ jsb-14/js/main.js
@@ -0,0 +1,29 @@
+const tagsInterpreter = {
+    title: "h1",
+    subtitle: "h2",
+    body: "p",
+}
+
+function awfulCreateNodeMethod(tagName, content) {
+    return `<${tagName}>${content}</${tagName}>`;
+}
+
+function updateBody(data) {
+    let innerHTML = ""
+    for (const key in data) {
+        innerHTML += awfulCreateNodeMethod(tagsInterpreter[key], data[key])
+    }
+    document.body.innerHTML = innerHTML;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    request({
+        url: "./index.json",
+        onSuccess: (data) => {
+            updateBody(data);
+        },
+        onError: (message) => {
+            console.log(message);
+        }
+    });
+});
